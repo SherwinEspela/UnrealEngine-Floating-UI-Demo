dae10f90e23e050f6e02d2cab110c5013e9a81d6
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2023-11-09 10:55:44 -0800

    Adding Unreal project source

---
+++ WorldSpaceUIDemo/Source/WorldSpaceUIDemo/WorldSpaceUIDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WorldSpaceUIDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WorldSpaceUIDemo, "WorldSpaceUIDemo" );
